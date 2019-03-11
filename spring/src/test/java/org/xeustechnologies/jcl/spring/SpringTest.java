1873ba6b48fe1bca61c72c6c4447f7103093362e
michael <michael@weimob.com>
2019-03-11 14:16:48 +0800

    refactor:去掉web

--- spring/src/test/java/org/xeustechnologies/jcl/spring/SpringTest.java
+++ spring/src/test/java/org/xeustechnologies/jcl/spring/SpringTest.java
@@ -11,17 +11,16 @@ import java.lang.reflect.InvocationTargetException;
 @RunWith(JUnit4.class)
 public class SpringTest extends TestCase {
     @Test
-    public void testWithSpring() throws ClassNotFoundException, IllegalArgumentException, SecurityException,
-            IllegalAccessException, InvocationTargetException, NoSuchMethodException {
-        ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext( "classpath:spring-test.xml" );
+    public void testWithSpring() throws IllegalArgumentException, SecurityException {
+        ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext("classpath:spring-test.xml");
 
         // Bean loaded with JCL
-        Object test1 = appContext.getBean( "test1" );
+        Object test1 = appContext.getBean("test1");
 
         // Bean loaded with parent CL
-        Object test2 = appContext.getBean( "test2" );
+        Object test2 = appContext.getBean("test2");
 
-        assertEquals( "org.xeustechnologies.jcl.loader.JarClassLoader", test1.getClass().getClassLoader().getClass().getName() );
-        assertEquals( "sun.misc.Launcher$AppClassLoader", test2.getClass().getClassLoader().getClass().getName() );
+        assertEquals("org.xeustechnologies.jcl.loader.JarClassLoader", test1.getClass().getClassLoader().getClass().getName());
+        assertEquals("sun.misc.Launcher$AppClassLoader", test2.getClass().getClassLoader().getClass().getName());
     }
 }
